150f8070c88e8551660f770e80bf6df5508793de
PedroHenriqueLira <pedrohenriquecardoso99@hotmail.com>
2022-07-25 22:58:48 -0300

    Exercicio Strings

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,7 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(`sem o comando trim fica ${minhaString.length} caracteres`)
+console.log(minhaString.trim())
+const quantidadeDeCaracteres = minhaString.trim()
+console.log(`com o comando trim fica ${quantidadeDeCaracteres.length} caracteres`)
+const minhaStringSubstituida = minhaString.replace('________.      ','sticioso.')
+console.log(minhaStringSubstituida)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,8 @@
 //Crie a const para a frase aqui
+let casa = 'verde'
+let portão = 'azul'
+
+const frase = `Jorge tem uma casa ${casa} e com portão ${portão}, com os dizeres: "BOAS VINDAS,`+ ("mas não deixe o gato sair").toUpperCase()
+console.log(frase)
+
+console.log(frase.replace('verde','rosa').replace('azul','laranja'))
