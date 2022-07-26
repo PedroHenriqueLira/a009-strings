79f653e398ab8f6c606e5aa60c326b342eddb22f
PedroHenriqueLira <pedrohenriquecardoso99@hotmail.com>
2022-07-26 20:48:51 -0300

    Atualizacao Exercicio String

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,8 +1,11 @@
 //Crie a const para a frase aqui
-let casa = 'verde'
-let portão = 'azul'
 
-const frase = `Jorge tem uma casa ${casa} e com portão ${portão}, com os dizeres: "BOAS VINDAS,`+ ("mas não deixe o gato sair").toUpperCase()
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS,`+ ("mas não deixe o gato sair").toUpperCase()
 console.log(frase)
 
 console.log(frase.replace('verde','rosa').replace('azul','laranja'))
+
+const fraseNova = frase
+const temVerde = fraseNova.includes('verde')
+const temLaranja = fraseNova.includes('laranja')
+console.log(`na frase a cima inclui verde? ${temVerde}\nna frase a cima inclui laranja? ${temLaranja} `)
